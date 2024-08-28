69b36d67076d5e96b019225155735a98442f72da
Shravan <shravanaligar@gmail.com>
2024-08-28 13:19:03 +0530

    My

--- JDBC.java
+++ JDBC.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+// JDBC Program 
 public class JavaJDBC {
     public static void main(String[] args) {
         try {
